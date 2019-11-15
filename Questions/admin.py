11626ea0aa11a12b0f58daff434b6b5e91443afa
trujiyo <struj018@gmail.com>
2019-11-14 22:30:47 -0500

    question and response form created

--- Questions/admin.py
+++ Questions/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Question
+#from .models import Question
 # Register your models here.
+'''
 class QuestionAdmin(admin.ModelAdmin):
     list_display = ('User', 'Question', 'Description')
 admin.site.register(Question, QuestionAdmin)
+'''
