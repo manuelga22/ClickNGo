038fc63e026ef5e8ede911a55f5a1a1fb472df46
Miguel Wolmers <rakasmurfs@gmail.com>
2019-11-16 16:44:46 -0500

    resolved Profile vs master conflicts

--- StorePage/views.py
+++ StorePage/views.py
@@ -1,9 +1,6 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
-<<<<<<< HEAD
-from .models import Profile 
-=======
->>>>>>> 67b354d9c77c63bd7632717a7be0711a04cc8e07
+# from .models import Profile 
 # Create your views here.
 def index(request):
     User = request.user
